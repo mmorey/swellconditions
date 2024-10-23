c2df2d3b466dc35f043bc5468dc25dfb1acfa1a8
Matthew Morey <me@matthewmorey.com>
2024-10-22 17:24:59 -0700

    Added comments to the data types for tides

--- src/APIClients/TidesAndCurrentsGovTypes.ts
+++ src/APIClients/TidesAndCurrentsGovTypes.ts
@@ -9,9 +9,12 @@ export interface TidesAndCurrentsGovWaterTemperatureAPIResponse {
 }
 
 export interface WaterTemperatureReading {
-  t: string;
-  v: string;
-  f: string;
+  t: string; // Time - Date and time of the observation, Greenwich Mean Time (GMT)
+  v: string; // Value - Measured water temperature
+  f: string; // Data Flags - in order of listing:
+  //-- (X) A flag that when set to 1 indicates that the maximum expected water temperature was exceeded
+  //-- (N) A flag that when set to 1 indicates that the minimum expected water temperature was exceeded
+  //-- (R) A flag that when set to 1 indicates that the rate of change tolerance limit was exceeded
 }
 
 export interface TideStation {
@@ -32,11 +35,11 @@ export interface TidesAndCurrentsGovWaterLevelAPIResponse {
 }
 
 export interface WaterLevelReading {
-  t: string;
-  v: string;
-  s: string;
-  f: string;
-  q: string;
+  t: string; // Time - Date and time of the observation, Greenwich Mean Time (GMT)
+  v: string; // Value - Measured water level height
+  s: string; // Sigma - Standard deviation of 1 second samples used to compute the water level height
+  f: string; // Data Flags
+  q: string; // Quality Assurance/Quality Control level, -- p = preliminary, -- v = verified
 }
 
 export interface TidesAndCurrentsGovTideHiLoPredictionAPIResponse {
@@ -44,9 +47,9 @@ export interface TidesAndCurrentsGovTideHiLoPredictionAPIResponse {
 }
 
 export interface TidePredictionReading {
-  t: string;
-  v: string;
-  type: string;
+  t: string; // Time - Date and time of the observation, Greenwich Mean Time (GMT)
+  v: string; // Value - Predicted water level height
+  type: string; // Type - Designation of Water level height. HH = Higher High water, H = High water, L = Low water, LL = Lower Low water
 }
 
 export interface TidesAndCurrentsGovTideDetailedPredictionAPIResponse {
@@ -54,8 +57,8 @@ export interface TidesAndCurrentsGovTideDetailedPredictionAPIResponse {
 }
 
 export interface DetailedTidePredictionReading {
-  t: string;
-  v: string;
+  t: string; // Time - Date and time of the observation, Greenwich Mean Time (GMT)
+  v: string; // Value - Predicted water level height
 }
 
 export interface WaterLevelData {
